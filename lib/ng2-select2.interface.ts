66ff7f3624dd7fb315cf46e1095d5934fd359a18
Pi-Bouf <pierre.bouffier05@gmail.com>
2020-10-30 16:49:34 +0100

    Updated ng2-select2 version

--- lib/ng2-select2.interface.ts
+++ lib/ng2-select2.interface.ts
@@ -2,7 +2,7 @@ export interface Select2OptionData {
     id: string;
     text: string;
     disabled?: boolean;
-    children?: Array<Select2OptionData>;
+    children?: Select2OptionData[];
     additional?: any;
 }
 
